e000f17517e92fcb452ec00f8457421e436afde8
plsinduja <polisettysindhuja@gmail.com>
2023-08-14 15:30:10 +0300

    install tailwind css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Head from './Head'
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Google Clone',
-  description: 'Google clone created by Next js 13',
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <Head />
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>HOME</div>;
+  return <div className="text-red-600 text-3xl">HOME</div>;
 }
