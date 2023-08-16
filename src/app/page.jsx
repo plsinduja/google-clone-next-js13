fc9f1bcc0ce1101388b36cbc9db2822f7e6d11fc
plsinduja <polisettysindhuja@gmail.com>
2023-08-16 16:49:13 +0300

    adding body content f home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,20 @@
 import HomeHeader from "@/components/HomeHeader";
+import Image from "next/image";
+import HomeSearch from "@/components/HomeSearch";
 export default function Home() {
   return (
     <>
-    <HomeHeader />
+      {/* header */}
+      <HomeHeader />
+      {/* body */}
+      <div className="flex flex-col mt-24 items-center">
+        <Image
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+        alt="image not found"/>
+        <HomeSearch />
+      </div>
     </>
-  )
+  );
 }
