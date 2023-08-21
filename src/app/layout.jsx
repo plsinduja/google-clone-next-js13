d8298f0d8ee23e8ac0f2e279bea62ba4bbd37ba2
plsinduja <polisettysindhuja@gmail.com>
2023-08-21 14:29:22 +0300

    add footer component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,15 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import Footer from "@/components/Footer";
 import Head from './Head'
-const inter = Inter({ subsets: ['latin'] })
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <Head />
-      <body className={inter.className}>{children}</body>
+      <body>
+      {children}
+      {/* Footer */}
+      <Footer />
+      </body>
     </html>
   )
 }
