2cb4832de9f26908458ec339d93d337ace100c10
plsinduja <polisettysindhuja@gmail.com>
2023-08-29 11:52:50 +0300

    add pagination component

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,10 @@
 import ImageSearchResults from "@/components/ImageSearchResults";
 import Link from "next/link";
 export default async function ImageSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || "1";
   await new Promise((resolve) => setTimeout(resolve,5000));
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
   );
   if (!response.ok) {
     throw new Error("failed to fetch data");
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,10 @@
 import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 export default async function WebSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || "1";
   await new Promise((resolve) => setTimeout(resolve,5000));
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
   );
   if (!response.ok) {
     throw new Error("failed to fetch data");
